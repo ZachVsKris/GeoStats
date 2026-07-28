1eca135096073a46811f961c2ce22b6a93e67e00
ZachVsKris <114275017+ZachVsKris@users.noreply.github.com>
2026-07-28 01:22:38 -0700

    Delete app directory

--- app/daily/page.tsx
+++
@@ -1,7 +0,0 @@
-import GeoSecondComingGame from "../../components/GeoSecondComingGame";
-
-export const metadata = { title: "Scout Daily | GeoStats" };
-
-export default function DailyPage() {
-  return <GeoSecondComingGame initialDifficulty="easy" />;
-}
--- app/random/expert/page.tsx
+++
@@ -1,7 +0,0 @@
-import GeoSecondComingGame from "../../../components/GeoSecondComingGame";
-
-export const metadata = { title: "Expert Random Test | GeoStats" };
-
-export default function RandomExpertPage() {
-  return <GeoSecondComingGame initialDifficulty="expert" mode="random" />;
-}

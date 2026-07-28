ad294efaf500670a3654779c9101eb1ebb721657
ZachVsKris <114275017+ZachVsKris@users.noreply.github.com>
2026-07-28 01:22:53 -0700

    Delete lib directory

--- lib/version.ts
+++
@@ -1,5 +0,0 @@
-export const APP_VERSION = "15.1.0";
-export const DATASET_VERSION = "2026-07-27-category-review-v15";
-export const RULES_VERSION = "9.0";
-export const CATEGORY_SET_VERSION = "SCOUT-ADVENTURER-EXPERT-V15-CURATED-CATALOG";
-export const MAX_YEAR_SPREAD = 3;

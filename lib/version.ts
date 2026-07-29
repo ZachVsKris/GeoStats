306154b24c3f7aa3c0ff3209552da756fbad0bb7
ZachVsKris <114275017+ZachVsKris@users.noreply.github.com>
2026-07-28 18:20:49 -0700

    Delete lib directory

--- lib/version.ts
+++
@@ -1,5 +0,0 @@
-export const APP_VERSION = "15.4.0";
-export const DATASET_VERSION = "2026-07-28-strategy-diversity-v15.4";
-export const RULES_VERSION = "11.0";
-export const CATEGORY_SET_VERSION = "SCOUT-ADVENTURER-EXPERT-V15-4-RUNTIME-TIERS";
-export const MAX_YEAR_SPREAD = 3;

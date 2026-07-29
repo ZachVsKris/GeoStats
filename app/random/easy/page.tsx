f56901298201ab36bc776d73fa491d3717c3fcb9
ZachVsKris <114275017+ZachVsKris@users.noreply.github.com>
2026-07-28 18:19:57 -0700

    Delete app directory

--- app/random/easy/page.tsx
+++
@@ -1,7 +0,0 @@
-import GeoSecondComingGame from "../../../components/GeoSecondComingGame";
-
-export const metadata = { title: "Scout Random Test | GeoStats" };
-
-export default function RandomEasyPage() {
-  return <GeoSecondComingGame initialDifficulty="easy" mode="random" />;
-}
--- app/random/page.tsx
+++
@@ -1,7 +0,0 @@
-import GeoSecondComingGame from "../../components/GeoSecondComingGame";
-
-export const metadata = { title: "Adventurer Random Test | GeoStats" };
-
-export default function RandomPage() {
-  return <GeoSecondComingGame initialDifficulty="normal" mode="random" />;
-}

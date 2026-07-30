fa212bfddff341efea5513692eca5478a2df3c71
ZachVsKris <114275017+ZachVsKris@users.noreply.github.com>
2026-07-29 17:52:50 -0700

    Add files via upload

---
+++ app/daily/adventurer/page.tsx
@@ -0,0 +1,7 @@
+import GeoSecondComingGame from "../../../components/GeoSecondComingGame";
+
+export const metadata = { title: "Adventurer Daily | GeoStats" };
+
+export default function AdventurerDailyPage() {
+  return <GeoSecondComingGame initialDifficulty="normal" />;
+}

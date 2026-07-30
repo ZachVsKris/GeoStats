cfee754ebb659177cdf33bc488a9e5b21b745f2f
ZachVsKris <114275017+ZachVsKris@users.noreply.github.com>
2026-07-30 10:34:17 -0700

    Delete lib directory

--- lib/version.ts
+++
@@ -1,5 +0,0 @@
-export const APP_VERSION = "15.8.0";
-export const DATASET_VERSION = "2026-07-29-expansion-intake-review-v15.8.0";
-export const RULES_VERSION = "13.1";
-export const CATEGORY_SET_VERSION = "SCOUT-ADVENTURER-EXPERT-V15-8-EXPANSION";
-export const MAX_YEAR_SPREAD = 3;

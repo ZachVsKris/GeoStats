937cac6accc3005782fe0c94da435046a3723b68
ZachVsKris <114275017+ZachVsKris@users.noreply.github.com>
2026-08-05 15:56:41 -0700

    Delete app directory

--- app/daily/adventurer/page.tsx
+++
@@ -1,7 +0,0 @@
-import GeoSecondComingGame from "../../../components/GeoSecondComingGame";
-
-export const metadata = { title: "Adventurer Daily | GeoStats" };
-
-export default function AdventurerDailyPage() {
-  return <GeoSecondComingGame initialDifficulty="normal" />;
-}

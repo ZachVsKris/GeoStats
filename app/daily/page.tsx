c690d9b39ab8f412533958e525837867938848f9
ZachVsKris <114275017+ZachVsKris@users.noreply.github.com>
2026-08-24 22:59:26 -0700

    Add files via upload

---
+++ app/daily/page.tsx
@@ -0,0 +1,13 @@
+import GeoSecondComingGame from "../../components/GeoSecondComingGame";
+import { loadPublicDailyPayload } from "../../lib/publicDaily";
+import { newYorkDate } from "../../lib/time";
+
+export const dynamic = "force-dynamic";
+
+export const metadata = { title: "Scout Daily | GeoStats" };
+
+export default async function DailyPage() {
+  const date = newYorkDate();
+  const initialDailyPayload = await loadPublicDailyPayload(date);
+  return <GeoSecondComingGame initialDifficulty="easy" initialDailyDate={date} initialDailyPayload={initialDailyPayload ?? undefined} />;
+}
